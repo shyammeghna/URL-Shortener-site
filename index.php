09f956e63513fec5a231889431ecdc07665007a5
Shiyamaladevi Pachaiappan <45041811+shyammeghna@users.noreply.github.com>
2020-02-13 16:59:49 +0100

    update
    update

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 					url: url
 				}, function(data, textStatus, xhr) {
 					var od = data;
-					data = 'http://downloadmycode.com/short?id=' + data;
+					data = 'http://mycode.com/short?id=' + data;
 					$('.errors').html('<a href="' + data + '" target="_blank">sho.rt/' + od + '</a>')
 				});
 			});
